f86960c7819c61da1818752835f0cf9926d1f70d
Juliano Grams <juliano.grams@gmail.com>
2015-09-25 22:28:44 -0300

    Start to resolve

--- TrabalhoMergeSort/src/br/univel/sorter/QuickSort.java
+++ TrabalhoMergeSort/src/br/univel/sorter/QuickSort.java
@@ -14,33 +14,62 @@ public class QuickSort extends Sort{
 	}
 	
 	private void quickSort(Object[] arr, int left, int right){
-		int idx = partition(arr, left, right);
-		if(left < idx-1)
-			quickSort(arr, left, idx-1);
+		 int stack[] = new int[right - left + 1];
+		 
+		    // initialize top of stack
+		    int top = -1;
+		 
+		    // push initial values of l and h to stack
+		    stack[ ++top ] = left;
+		    stack[ ++top ] = right;
+		 
+		    // Keep popping from stack while is not empty
+		    while ( top >= 0 )
+		    {
+		        // Pop h and l
+		        right = stack[ top-- ];
+		        left = stack[ top-- ];
+		 
+		        // Set pivot element at its correct position in sorted array
+		        int p = partition( arr, left, right );
+		 
+		        // If there are elements on left side of pivot, then push left
+		        // side to stack
+		        if ( p-1 > left )
+		        {
+		            stack[ ++top ] = left;
+		            stack[ ++top ] = p - 1;
+		        }
+		 
+		        // If there are elements on right side of pivot, then push right
+		        // side to stack
+		        if ( p+1 < right )
+		        {
+		            stack[ ++top ] = p + 1;
+		            stack[ ++top ] = right;
+		        }
+		    }
 		
-		if(right > idx)
-			quickSort(arr, idx, right);
 	}
 	private int partition(Object[] numbers, int left, int right) {
 		
-		int pivot = (int)numbers[left];
+		int x = (int)numbers[right];
+	    int i = (left - 1);
+	 
+	    for (int j = left; j <= right- 1; j++){
+	        if ((int)numbers[j] <= x){
+	            i++;
+	            int tmp = (int)numbers[i];
+	            numbers[i]= numbers[j];
+	            numbers[j] = tmp;
+	        }
+	    }
+	    int tmp = (int)numbers[i+1];
+        numbers[i+1]= numbers[right];
+        numbers[right] = tmp;
+	    return (i + 1);
 		
-		while (left <= right) {
-			while ((int)numbers[left] < pivot) {
-				left++;
-			}
-			while ((int)numbers[right] > pivot) {
-				right--;
-			}
-			if (left <= right) {
-				int tmp = (int)numbers[left];
-				numbers[left] = numbers[right];
-				numbers[right]=tmp;
-				left++;
-				right--;
-			}
-		}
-		return left;
+		
 	}
 	
 }
