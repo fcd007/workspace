3705b9605ce97a875a3cbc36af7dd5b416a3905d
Juliano Grams <juliano.grams@gmail.com>
2015-09-25 23:43:11 -0300

    quase pronto

--- TrabalhoMergeSort/src/br/univel/sorter/QuickSort.java
+++ TrabalhoMergeSort/src/br/univel/sorter/QuickSort.java
@@ -10,43 +10,53 @@ public class QuickSort extends Sort {
 
 	@Override
 	public void sort(ArrayList array) {
-		
+
 		int[] numbers = Sort.convertIntegers(array);
-		int start = 0;
-		int end = numbers.length-1;
-		quickSort(numbers, start, end);
+		quickSort(numbers);
 	}
 
-	private void quickSort(int[] arr, int left, int right) {
-		int idx = partition(arr, left, right);
-		if (left < idx - 1) {
-			quickSort(arr, left, idx - 1);
-		}
-		if (right > idx) {
-			quickSort(arr, idx, right);
-		}
+	private void quickSort(int[] arr) {
+		int[] skip = new int[arr.length+1];
+	      int i, j, sortedCount = 0;
+	      while (sortedCount < arr.length) {
+	        for (i=0; i<arr.length; i++)
+	          if (skip[i] == 0) {
+	            for (j=i; (j<arr.length-1) && (skip[j+1] == 0); j++) ;
+	            for ( ; i<=j; i++) {
+	              skip[i = partition(arr,i,j)] = 1;
+	              sortedCount++;
+	            }
+	          }
+	          else {
+	            // skip[i] += skip[i + skip[i]];
+	            while ((j = skip[i+skip[i]]) > 0) skip[i] += j;
+	            i += skip[i]-1;
+	          }
+	      }
 
 	}
 
 	private int partition(int[] numbers, int left, int right) {
 
-		int pivot = numbers[left];
-		while (left <= right) {
-			while (numbers[left] < pivot) {
-				left++;
-			}
-			while (numbers[right] > pivot) {
-				right--;
-			}
-			if (left <= right) {
-				int tmp = numbers[left];
-				numbers[left] = numbers[right];
-				numbers[right] = tmp;
-				left++;
-				right--;
-			}
-		}
-		return left;
+		
+        if (left == right) return left;
+        int i = left - 1;
+        int j = right;
+        while (true) {
+            while (numbers[++i] < numbers[right]);                        
+            while (numbers[right] < numbers[--j])     
+                if (j == left) break;     
+            if (i >= j) break;              
+            swap(numbers, i, j);                
+        }
+        swap(numbers, i, right);                
+        return i;
 	}
+	
+	private void swap(int[] a, int i, int j) {
+	      int temp = a[i];
+	      a[i] = a[j];
+	      a[j] = temp;
+	    }
 
 }
