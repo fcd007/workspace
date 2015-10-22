354727e30c95587efbd888accdb7152ea7c82c2c
julianograms <julianograms@gmail.com>
2015-10-21 22:19:17 -0200

    Treee

---
+++ ExpressionTree/src/br/univel/aulaTree/Principal.java
@@ -0,0 +1,12 @@
+package br.univel.aulaTree;
+
+public class Principal {
+	public static void main(String[] args) {
+		Tree tree = new Tree();
+		tree.insert(5);
+		tree.insert(4);
+		tree.insert(3);
+		tree.insert(2);
+		tree.insert(10);
+	}
+}
