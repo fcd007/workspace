0c1ea494b539225a863884f0a63c0e57d631f289
Juliano Montezano Grams <julianograms@gmail.com>
2015-10-28 22:31:13 -0200

    Teacher solution Expression Tree

--- ExpressionTree/src/br/univel/aulaTree/Principal.java
+++ ExpressionTree/src/br/univel/aulaTree/Principal.java
@@ -5,8 +5,9 @@ public class Principal {
 		Tree tree = new Tree();
 		tree.insert(5);
 		tree.insert(4);
-		tree.insert(3);
+		tree.insert(4);
 		tree.insert(2);
 		tree.insert(10);
+		tree.showPrefix();
 	}
 }
