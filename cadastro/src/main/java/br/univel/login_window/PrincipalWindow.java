60573251338124446c41e2e88495acbe3ae9fa53
Juliano Grams <juliano.grams@gmail.com>
2015-11-02 23:48:20 -0200

    Locadora

--- cadastro/src/main/java/br/univel/login_window/PrincipalWindow.java
+++ cadastro/src/main/java/br/univel/login_window/PrincipalWindow.java
@@ -12,6 +12,8 @@ import javax.swing.JMenuItem;
 import javax.swing.JTabbedPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PrincipalWindow extends JFrame {
 
@@ -68,6 +70,15 @@ public class PrincipalWindow extends JFrame {
 			}
 		});
 		mnCadastro.add(mntmCliente);
+		
+		JMenuItem mntmGenero = new JMenuItem("Genero");
+		mntmGenero.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				openGeneroTab();
+			}
+		});
+		mnCadastro.add(mntmGenero);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -76,5 +87,21 @@ public class PrincipalWindow extends JFrame {
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		contentPane.add(tabbedPane, BorderLayout.CENTER);
 	}
+
+	protected void openGeneroTab() {
+		TelaCadastroCliente telaCadastroCliente = new TelaCadastroCliente();
+		ActionListener action = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				tabbedPane.remove(telaCadastroCliente);
+				
+			}
+		};
+//		telaCadastroCliente.setCloseAction(action);
+		telaCadastroCliente.setCloseAction(e -> tabbedPane.remove(telaCadastroCliente));
+		tabbedPane.addTab("Cliente", telaCadastroCliente);
+		
+	}
+	
 
 }
