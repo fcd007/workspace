48c0d6ac7008b76f2e31adab2244948436bd0a4e
Juliano Montezano Grams <julianograms@gmail.com>
2016-06-23 00:40:31 -0300

    Filtro fixo

--- webpack/app/components/ContactList/ContactsApp.js
+++ webpack/app/components/ContactList/ContactsApp.js
@@ -1,18 +1,25 @@
 import React, {Component, PropTypes} from 'react';
-import {render} from 'react-dom';
-
 
 class ContactsApp extends Component{
+	constructor(){
+		super();
+		this.state={
+			filterText: ''
+		};
+	}
 	render(){
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar filterText={this.state.filterText}/>
 				<ContactList contacts={this.props.contacts}/>
 			</div>
 		);
 	}
 }
 
+ContactsApp.propTypes = {
+	contacts: PropTypes.arrayOf(PropTypes.object)
+}
 
 ContactsApp.defaultProps = {
   contacts: [
@@ -22,26 +29,34 @@ ContactsApp.defaultProps = {
 	]
 };
 
-ContactsApp.propTypes = {
-	contacts: PropTypes.arrayOf(PropTypes.object)
-}
+
 
 class SearchBar extends Component{
 	render(){
-		return (<input type="search" placeholder="search" />);
+		return (
+			<input  type="search" placeholder="search"
+							value={this.props.filterText} />
+		);
 	}
+}
+
+SearchBar.propTypes = {
+	filterText: PropTypes.string.isRequired
 }
 
 class ContactList extends Component{
 	render(){
+		let filteredContacts = this.props.contacts.filter(
+			(contact) => contact.name.indexOf(this.props.filterText) !== -1
+		);
 		return(
 			<ul>
-				{this.props.contacts.map(
+				{filteredContacts.map(
 					(contact) => <ContactItem key={contact.email}
 																		name={contact.name}
 																		email={contact.email}
-																		/>
-					)}
+																		/>)
+				}
 			</ul>
 			);
 	}
@@ -51,7 +66,7 @@ ContactList.propTypes = {
 	contacts: PropTypes.arrayOf(PropTypes.object)
 }
 
-class ContactItem extends Components{
+class ContactItem extends Component{
 	render(){
 		return (<li>{this.props.name} - {this.props.email}</li>);
 	}
